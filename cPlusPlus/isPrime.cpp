2ec47e29f3b599b5b43ca30317cd235fb5ff090b
Ivan <ivanmp@live.com>
2016-01-08 14:14:44 -0700

    add program for prime check

--- cPlusPlus/isPrime.cpp
+++ cPlusPlus/isPrime.cpp
@@ -1,52 +1,43 @@
-#include <iostream>
-
-using namespace std;
-int main()
-{
-	bool prime;
-
-	// If number is less than 2 it's not prime ex.{-Z},0,1
-	if (number < 2) 
-		prime = false;
-	// if number equals 2 than it is prime
-	if (number == 2)
-		prime = true;
-	// if number is divisible by two with 0 remainder then not prime
-    if(number % 2 == 0) 
-    	prime = false;
-
-    for(int i=3; (i*i) <= number; i+=2)
-    {
-        if(number % i == 0 ) 
-        	prime = false;
-    }
-
-    prime = true;
-
-}
-
-/
-
+// sample program demonstrating simple prime test.
+// author: ivan
 
 // Contains prototypes for cout & cin
 #include <iostream>
+// purely for demonstration purposes
+using namespace std; 
 
 // Prototype for IsPrime function.
 bool IsPrime(int);
 
-// main function is always run first in a
-// C++ program.
+// PRE: takes integer as argument for primality test
+// POST: returns true if integer is prime otherwise false 
+bool IsPrime(int number)
+{	
+    // If number is less than 2 it's not prime i.e.{-Z},0,1
+	if (number < 2)
+        return false;
+    // if number equals 2 than it is prime
+	if (number == 2)
+		return true;
+	// if number is divisible by two with 0 remainder then not prime
+    if(number % 2 == 0) 
+    	return false;
+    // check if number is divisible by or odd; previous flow of control removed remaining sets 
+    for(int i=3; (i*i) <= number; i+=2)
+    {
+        if(number % i == 0) 
+        	return false;
+    }
+	return true;	
+}
 
+// Main Procedure
 int main()
-{	//Given:   nothing.
-	//Results: Accepts a number and
-	//		  indicates if it is prime.
-
+{
 	int number;
-	
-	cout << "Enter an integer (>1): ";
+	cout << "Enter an integer: ";
 	cin >> number;
-	
+    
 	if (IsPrime(number))
 	{
 		cout << number << " is prime." << endl;
@@ -55,27 +46,6 @@ int main()
 	{
 		cout << number << " is not prime." << endl;
 	}
-
-	// This is more convention than anything.
+    
 	return 0;
 }
-
-bool IsPrime(int number)
-{	// Given:   num an integer > 1
-	// Returns: true if num is prime
-	// 			false otherwise.
-	
-	int i;
-	
-	for (i=2; i<number; i++)
-	{
-		if (number % i == 0)
-		{
-			return false;
-		}
-	}
-	
-	return true;	
-}
-
-*/
