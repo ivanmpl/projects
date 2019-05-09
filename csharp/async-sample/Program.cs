34a60d61f6752dcd09b4dc5c4b0d6b1cc832a4fa
Ivan <ivanmp@live.com>
2019-05-09 18:52:00 -0400

    add async and linq example projects

---
+++ csharp/async-sample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace async_sample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MakeBreakfast();
+            GetWebResponses();
+            Console.ReadLine();
+        }
+
+        #region breakfast
+        public static async void MakeBreakfast()
+        {
+            Task<string> coffeeTask = MakeCoffee();
+            System.Console.WriteLine("doing other stuff ...");
+            string coffee = await coffeeTask;
+            System.Console.WriteLine(coffee);
+        }
+
+        public static async Task<string> MakeCoffee()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    System.Console.WriteLine("making coffee..");
+                }
+            });
+            return "coffee ready";
+        }
+        #endregion
+        public static async void GetWebResponses()
+        {
+            Task<string> webTask = ProcessURLAsync("http://www.contoso.com/");
+            string response = await webTask;
+            System.Console.WriteLine(response);
+        }
+
+        public static async Task<string> ProcessURLAsync(string url)
+        {
+            try
+            {
+                //HttpResponseMessage response = await client.GetAsync(url);
+                //response.EnsureSuccessStatusCode();
+                //string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                using (HttpClient client = new HttpClient())
+                {
+                System.Console.WriteLine("before client call");
+                string responseBody = await client.GetStringAsync(url);
+                System.Console.WriteLine("after client call");
+
+                System.Console.WriteLine(responseBody);
+                return responseBody;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return e.Message;
+            }
+        }
+
+    }
+}
---
+++ csharp/linq-examples/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace linq_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // data source samples
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            List<string> dinosaurs = new List<string>();
+            dinosaurs.Add("Tyrannosaurus");
+            dinosaurs.Add("Amargasaurus");
+            dinosaurs.Add("Mamenchisaurus");
+            dinosaurs.Add("Deinonychus");
+            dinosaurs.Add("Compsognathus");
+
+            // data source from an XML document.
+            XElement contries = XElement.Load(@"sample.xml");
+
+            // anonymous types
+            var anon = new { Age = 21, Name = "john" };
+
+            // query creation
+            IEnumerable<int> evenNumQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            // force immediate execution
+            List<int> evenNumList =
+                (from num in numbers
+                 where (num % 2) == 0
+                 select num).ToList();
+
+            IEnumerable<string> longDinoName =
+                from dino in dinosaurs
+                where dino.Length > 11
+                select dino;
+
+            IEnumerable<XElement> elements =
+                from el in contries.Elements("country")
+                where (int)el.Element("year") > 2010
+                select el;
+
+            var els =
+                from country in contries.Elements("country")
+                select new {country.Element("rank").Value};
+
+
+            // query execution
+            int evenNumCount = evenNumQuery.Count();
+
+            foreach (int num in evenNumQuery)
+            {
+                System.Console.Write(num + " ");
+            }
+            System.Console.WriteLine();
+
+            foreach (string dino in longDinoName)
+            {
+                System.Console.Write(dino + " ");
+            }
+            System.Console.WriteLine();
+
+            foreach (XElement item in elements)
+            {
+                System.Console.WriteLine(item.Element("rank").Value);
+            }
+
+            System.Console.WriteLine(anon.Age + " " + anon.Name);
+
+            foreach (var el in els)
+            {
+                System.Console.WriteLine(el.Value);
+            }
+
+
+
+        }
+    }
+}
