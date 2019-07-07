7ec552d21d77f221189621004007a1c54eb8dde9
Ivan <ivanmp@live.com>
2019-07-07 14:47:58 -0600

    add program to test deployment and regex

---
+++ csharp/deploy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Figgle;
+
+namespace Applications.ConsoleApps
+{
+    public class ConsoleParser
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter any text, followed by <Enter>:\n");
+            String text = Console.ReadLine();
+            ShowWords(text);
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadKey();
+        }
+
+        private static void ShowWords(String s)
+        {
+            String pattern = @"\w+";
+            var matches = Regex.Matches(s, pattern);
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("\nNo words were identified in your input.");
+            }
+            else
+            {
+                Console.WriteLine($"\nThere are {matches.Count} words in your string:");
+                for (int ctr = 0; ctr < matches.Count; ctr++)
+                {
+                    //$"   #{ctr,2}: '{matches[ctr].Value}' at position {matches[ctr].Index}";
+                    string word = $"{matches[ctr].Value}";
+                    Console.WriteLine(FiggleFonts.Standard.Render(word));
+                }
+            }
+        }
+    }
+}
