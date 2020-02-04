07dbd2c703a77d196ebed6db8e45c366b75195a7
Ivan <ivanmp@live.com>
2020-02-04 13:19:45 -0700

    add missing Lab 5 implementation

---
+++ csharp/exercises/Lab5/Exercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+write a program to caluculate and print the series of Xth powers of 2,where 0<=x<=10(1,2,4,8,16….)
+without using arithematic operators or mathematiacal functions(i.e,*,/,+,-,methods of  system.
+Math are not allowed)
+ */
+
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter the nth powers of 2 to display: ");
+            System.Console.Write("Where 0 <= n >= 10: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            if (n >= 0 && n <= 10)
+            {
+                GetNthPowers(2, n);
+            }
+        }
+
+        public static void GetNthPowers(int power, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    System.Console.WriteLine(power >> 1);
+                }
+
+                System.Console.WriteLine(power);
+                power = power << 1;
+            }
+        }
+    }
+}
---
+++ csharp/exercises/Lab5/Exercise2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class TextInput
+{
+    protected string value;
+
+    public virtual void Add(char c)
+    {
+        value = value + c;
+    }
+
+    public string GetValue()
+    {
+        return value;
+    }
+
+}
+
+public class NumericInput : TextInput
+{
+    public override void Add(char c)
+    {
+        if (Char.IsNumber(c))
+        {
+            value += c;
+        }
+
+    }
+
+}
+
+
+public abstract class MotorVehicle
+{
+
+    int fuel = 0;
+
+    // They ALL have fuel, so lets implement this for everybody.
+    public int getFuel()
+    {
+        return this.fuel;
+    }
+
+    // That can be very different, force them to provide their
+    // own implementation.
+    public abstract void run();
+}
+
+// My teammate complies and writes vehicle looking that way
+public class Car : MotorVehicle
+{
+    public override void run()
+    {
+        System.Console.WriteLine("Wrroooooooom");
+    }
+}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        string d = "tes";
+        d.Equals("tsest", StringComparison.InvariantCultureIgnoreCase);
+
+        NumericInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
+
+        Car car = new Car();
+        car.run();
+    }
+}
