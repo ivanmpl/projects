d559dd876a40582215fbf813a28462a0391b370c
Ivan <ivanmp@live.com>
2020-04-30 22:15:55 -0600

    update guess number method name and refactor

--- python/random/guess_number.py
+++ python/random/guess_number.py
@@ -2,23 +2,20 @@
 import random
 import sys
 
-# sample function that takes number guess from standard input
 
-
-def guessNumber():
-    print("Please Enter a number between 1-10:")
-    user_guess = input()
+# function that takes number guess from standard input
+def guess_number():
+    user_guess = input("Please Enter a number between 1-10: ")
     actual_num = random.randint(1, 10)
-    print("Your guess: ", user_guess)
-    print("actual number: ", actual_num)
+    print("Your guess:", user_guess)
+    print("actual number:", actual_num)
     if user_guess == actual_num:
         print("You guessed correctly")
     else:
         print("Sorry you guessed incorrectly")
 
+
 # main() function
-
-
 def main():
     #  Command line args are in sys.argv[1], sys.argv[2] ...
     #  sys.argv[0] is the script name itself and can be ignored
@@ -28,21 +25,19 @@ def main():
         print("Hello")
 
     print("This program allows a user to guess a random number")
-    print("Press any key and Enter to Continue: ")
-    input()
-    guessNumber()
+    input("Press any key and Enter to Continue: ")
+    guess_number()
 
     while True:
-        print("try again? y/n: "),
-        answer = input()
+        answer = input("try again? y/n: ")
         if answer == 'y':
-            guessNumber()
+            guess_number()
         elif answer == 'n':
             exit()
         else:
             print("invalid response please try again")
 
 
-# Standard boilerplate to call the guessNumber() function
+# Standard boilerplate to call the guess_number() function
 if __name__ == '__main__':
     main()
